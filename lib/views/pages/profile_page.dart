efe02e5d2d3ff46780956374d12dc8a0a920eaff
Souvik Mandal <souvikmandals10@gmail.com>
2025-09-26 00:11:10 +0530

    scroll screeenig

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -11,40 +11,49 @@ class _ProfilePageState extends State<ProfilePage> {
   TextEditingController controller = TextEditingController();
   bool? isChecked = false;
   bool isSwitched = false;
+  double sliderValue = 0;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          TextField(
-            decoration: InputDecoration(border: OutlineInputBorder()),
-            onEditingComplete: () {
-              setState(() {});
-            },
-          ),
-          Text(controller.text),
-          Checkbox(tristate: true,value: isChecked, onChanged: (bool? value) {
-            setState(() {
-              isChecked = value!;
-            });
-          }),
-          CheckboxListTile(title: Text("Click"),value: isChecked, onChanged: (bool? value) {
-            setState(() {
-              isChecked = value!;
-            });
-          }),
-          Switch(value: isSwitched, onChanged: (bool value) {
-            setState(() {
-              isSwitched = value;
-            });
-          }),
-          SwitchListTile(title: Text('Switch Style'),value: isSwitched, onChanged: (bool value) {
-            setState(() {
-              isSwitched = value;
-            });
-          },)
-        ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(border: OutlineInputBorder()),
+              onEditingComplete: () {
+                setState(() {});
+              },
+            ),
+            Text(controller.text),
+            Checkbox(tristate: true,value: isChecked, onChanged: (bool? value) {
+              setState(() {
+                isChecked = value!;
+              });
+            }),
+            CheckboxListTile.adaptive(title: Text("Click"),value: isChecked, onChanged: (bool? value) {
+              setState(() {
+                isChecked = value!;
+              });
+            }),
+            Switch.adaptive(value: isSwitched, onChanged: (bool value) {
+              setState(() {
+                isSwitched = value;
+              });
+            }),
+            SwitchListTile.adaptive(title: Text('Switch Style'),value: isSwitched, onChanged: (bool value) {
+              setState(() {
+                isSwitched = value;
+              });
+            },),
+            Slider.adaptive(max: 10.0,value: sliderValue, onChanged: (value) {
+              setState(() {
+                sliderValue = value;
+              });
+            },),
+            Image.asset('assets/images/sample.jpg',height: 200,width: 200,fit: BoxFit.cover,),
+          ],
+        ),
       ),
     );
   }
