5ceb99ff0d3661c1695c26a03531e85ae02f62c9
Souvik Mandal <souvikmandals10@gmail.com>
2025-10-01 00:05:58 +0530

    fixing app rotating screen prob

--- lib/views/pages/login_page.dart
+++ lib/views/pages/login_page.dart
@@ -3,7 +3,9 @@ import 'package:ranapp/views/widget_tree.dart';
 import 'package:ranapp/widgets/hero_widget.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({super.key, required this.title});
+
+  final String title;
 
   @override
   State<LoginPage> createState() => _LoginPageState();
@@ -28,57 +30,64 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            HeroWidget(title: 'RanApp'),
-            SizedBox(height: 20.0),
-            TextField(
-              controller: controllerEmail,
-              decoration: InputDecoration(
-                hint: Text('Email'),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-              ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                HeroWidget(title: 'RanApp'),
+                SizedBox(height: 20.0),
+                TextField(
+                  controller: controllerEmail,
+                  decoration: InputDecoration(
+                    hint: Text('Email'),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
 
-              onEditingComplete: () {
-                setState(() {});
-              },
-            ),
-            SizedBox(height: 20.0),
-            TextField(
-              controller: controllerPw,
-              decoration: InputDecoration(
-                hint: Text('Password'),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
+                  onEditingComplete: () {
+                    setState(() {});
+                  },
                 ),
-              ),
+                SizedBox(height: 20.0),
+                TextField(
+                  controller: controllerPw,
+                  decoration: InputDecoration(
+                    hint: Text('Password'),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
 
-              onEditingComplete: () {
-                setState(() {});
-              },
+                  onEditingComplete: () {
+                    setState(() {});
+                  },
+                ),
+                SizedBox(height: 20.0),
+                FilledButton(
+                  onPressed: () {
+                    onLoginPressed();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(double.infinity, 40.0),
+                  ),
+                  child: Text(widget.title),
+                ),
+                SizedBox(height: 50.0),
+              ],
             ),
-            SizedBox(height: 20.0),
-            FilledButton(
-              onPressed: () {
-                onLoginPressed();
-              },
-              style: ElevatedButton.styleFrom(
-                minimumSize: Size(double.infinity, 40.0),
-              ),
-              child: Text('Login'),
-            ),
-          ],
+          ),
         ),
       ),
     );
   }
 
   void onLoginPressed() {
-    if (confirmedEmail == controllerEmail.text && confirmedPw == controllerPw.text) {
+    if (confirmedEmail == controllerEmail.text &&
+        confirmedPw == controllerPw.text) {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(
--- lib/views/pages/welcome_page.dart
+++ lib/views/pages/welcome_page.dart
@@ -8,54 +8,59 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            Lottie.asset('assets/lotties/welcome.json'),
-            Text(
-              'Ranapp',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 50.0,
-                letterSpacing: 50.0,
-              ),
-            ),
-            SizedBox(height: 20.0),
-            FilledButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  //pshReplacement thourgh pages forr login
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return LoginPage();
-                    },
+      body: Center(
+        child: SingleChildScrollView(     //for vertical rotation
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Lottie.asset('assets/lotties/welcome.json', height: 400.0),
+                Text(
+                  'Ranapp',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 50.0,
+                    letterSpacing: 50.0,
                   ),
-                );
-              },
-              style: FilledButton.styleFrom(
-                maximumSize: Size(double.infinity, 40.0),
-              ),
-              child: Text('Get Started'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return LoginPage();
-                    },
+                ),
+                SizedBox(height: 20.0),
+                FilledButton(
+                  onPressed: () {
+                    Navigator.push(
+                      //pshReplacement thourgh pages forr login
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return LoginPage(title: 'Register',);
+                        },
+                      ),
+                    );
+                  },
+                  style: FilledButton.styleFrom(
+                    maximumSize: Size(double.infinity, 40.0),
                   ),
-                );
-              },
-              style: FilledButton.styleFrom(
-                maximumSize: Size(double.infinity, 40.0),
-              ),
-              child: Text('Login'),
+                  child: Text('Get Started'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return LoginPage(title: 'Login',);
+                        },
+                      ),
+                    );
+                  },
+                  style: FilledButton.styleFrom(
+                    maximumSize: Size(double.infinity, 40.0),
+                  ),
+                  child: Text('Login'),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
