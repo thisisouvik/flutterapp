db6b6869a420022d53b5ff602c86503e054bd591
Souvik Mandal <souvikmandals10@gmail.com>
2025-10-02 18:07:29 +0530

    refactor: simplify HomePage layout using List.generate for ContainerWidgets

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -5,21 +5,26 @@ import 'package:ranapp/widgets/hero_widget.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-// HEroWidget for transition animation
+  // HEroWidget for transition animation
 
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: EdgeInsets.symmetric(horizontal: 20.0),
-    child: SingleChildScrollView(
-      child: Column(
-        children: [
-          HeroWidget(title: 'Ranapp',),
-          ContainerWidget(title: 'Basic Layout', description: 'This is a description',),
-          ContainerWidget(title: 'Basic Layout1', description: 'This is a description',),
-          ContainerWidget(title: 'Basic Layout2', description: 'This is a description',)
-        ],
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            HeroWidget(title: 'Ranapp'),
+            ...List.generate(5, (index) {
+              return ContainerWidget(
+                title: 'Basic Layout',
+                description: 'This is a description',
+              );
+            },
+            ),
+          ],
+        ),
       ),
-    ),
-    );  
+    );
   }
 }
