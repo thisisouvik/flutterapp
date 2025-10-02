4420c005551901fd4f5c985f21ad9f91ae3a8c1d
Souvik Mandal <souvikmandals10@gmail.com>
2025-10-03 00:06:37 +0530

    feat: refactor LoginPage layout to use LayoutBuilder and improve input field styling

--- lib/views/pages/login_page.dart
+++ lib/views/pages/login_page.dart
@@ -34,50 +34,57 @@ class _LoginPageState extends State<LoginPage> {
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(10.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Lottie.asset('assets/lotties/home.json', height: 400.0),
-                SizedBox(height: 20.0),
-                TextField(
-                  controller: controllerEmail,
-                  decoration: InputDecoration(
-                    hint: Text('Email'),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                  ),
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                return FractionallySizedBox(
+                  widthFactor: constraints.maxWidth > 1000 ? 0.5 : 1.0,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Lottie.asset('assets/lotties/home.json', height: 400.0),
+                      SizedBox(height: 20.0),
+                      TextField(
+                        controller: controllerEmail,
+                        decoration: InputDecoration(
+                          hint: Text('Email'),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
 
-                  onEditingComplete: () {
-                    setState(() {});
-                  },
-                ),
-                SizedBox(height: 20.0),
-                TextField(
-                  controller: controllerPw,
-                  decoration: InputDecoration(
-                    hint: Text('Password'),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                  ),
+                        onEditingComplete: () {
+                          setState(() {});
+                        },
+                      ),
+                      SizedBox(height: 20.0),
+                      TextField(
+                        controller: controllerPw,
+                        decoration: InputDecoration(
+                          hint: Text('Password'),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
 
-                  onEditingComplete: () {
-                    setState(() {});
-                  },
-                ),
-                SizedBox(height: 20.0),
-                FilledButton(
-                  onPressed: () {
-                    onLoginPressed();
-                  },
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(double.infinity, 40.0),
+                        onEditingComplete: () {
+                          setState(() {});
+                        },
+                      ),
+                      SizedBox(height: 20.0),
+                      FilledButton(
+                        onPressed: () {
+                          onLoginPressed();
+                        },
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: Size(double.infinity, 40.0),
+                        ),
+                        child: Text(widget.title),
+                      ),
+                      SizedBox(height: 50.0),
+                    ],
                   ),
-                  child: Text(widget.title),
-                ),
-                SizedBox(height: 50.0),
-              ],
+                );
+              },
             ),
           ),
         ),
@@ -95,7 +102,7 @@ class _LoginPageState extends State<LoginPage> {
             return WidgetTree(title: 'App');
           },
         ),
-        (route) => false,   //remove the one thats in the back
+        (route) => false, //remove the one thats in the back
       );
     }
   }
