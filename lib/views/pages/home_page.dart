74b5fe474d648fcdfbcf3b41410fa72d53c28246
Souvik Mandal <souvikmandals10@gmail.com>
2025-10-02 18:01:03 +0530

    feat: enhance ContainerWidget to accept title and description parameters

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -9,12 +9,14 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: EdgeInsets.all(16.0),
+    return Padding(padding: EdgeInsets.symmetric(horizontal: 20.0),
     child: SingleChildScrollView(
       child: Column(
         children: [
           HeroWidget(title: 'Ranapp',),
-          ContainerWidget()
+          ContainerWidget(title: 'Basic Layout', description: 'This is a description',),
+          ContainerWidget(title: 'Basic Layout1', description: 'This is a description',),
+          ContainerWidget(title: 'Basic Layout2', description: 'This is a description',)
         ],
       ),
     ),
--- lib/widgets/container_widget.dart
+++ lib/widgets/container_widget.dart
@@ -2,13 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:ranapp/data/constants.dart';
 
 class ContainerWidget extends StatelessWidget {
-  const ContainerWidget({super.key});
+  const ContainerWidget({
+    super.key,
+    required this.title,
+    required this.description,
+  });
+
+  final String title;
+  final String description;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      padding: EdgeInsets.symmetric(vertical: 10.0),
+      padding: EdgeInsets.only(top: 10.0),
       child: Card(
         child: Padding(
           padding: EdgeInsets.all(20.0),
